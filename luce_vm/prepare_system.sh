f2e565a4a2831431a88bda96299d37d3802c694a
Arno <angerer.arno@gmail.com>
2019-06-26 13:16:09 +1000

    Move functionality from bootstrap file to separte script
    I am still running into problems trying to run all the setup from the root user account which vagrant uses during initialization. Instead maybe easier to run the script manually after the machine has booted. That adds one more step but not worth to spend more time trying to solve this problem right now..
    
    For now use workaround and move on. Maybe re-visit this issue later. It definitely is possible to have it all provisioned by the root user but since so many packages, environment variables and commands are involved it is more complex.

---
+++ luce_vm/prepare_system.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# Bash commands to run after the VM has started
+
+# Install GNU compiler utitilities 
+# (required for web3 installation) 
+
+sudo apt --assume-yes install build-essential
+
+# ==== INSTALL ANACONDA ====
+
+# 1) Download installer script
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/vagrant/miniconda.sh
+
+# 2) Install miniconda
+bash /home/vagrant/miniconda.sh -b -p /home/vagrant/miniconda
+
+# 3) Add to path and initialize
+eval "$(/home/vagrant/miniconda/bin/conda shell.bash hook)"
+conda init
+source /home/vagrant/.bashrc
+
+# 4) Clean up 
+rm /home/vagrant/miniconda.sh
+
+# ==== CONDA ENVIRONMENT ====
+
+# Set up new conda environment
+conda create -y --name luce_vm python
+
+# Activate environment
+conda activate luce_vm
+
+# ==== INSTALL JUPYTER & KERNEL ====
+
+# Install Jupyter
+conda install -y jupyter
+
+# Set up jupyter kernel for luce python environment
+# pip install ipykernel
+python -m ipykernel install --user --name=conda_luce
+
+# ==== INSTALL WEB3 ====
+
+pip install web3
+pip install py-solc-x
+
+# ==== INSTALL SOLIDITY COMPILER ====
+
+# We use the py-colc-x package from the command line to install the solidity compiler
+# v0.4.25 is the latest stable v4 version available on github
+python -m solcx.install v0.4.25
+
+
+# ==== INSTALL NODE.JS AND GANACHE ====
+
+# 1) Install Node Version Manager
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# 2) Install node.js
+nvm install node 
+
+# 3) Install Ganache
+npm install -g ganache-cli
