5c616f940e4385a6384b09ed7bd9548ebfbd0b9d
Arno <angerer.arno@gmail.com>
2019-06-28 12:50:33 +1000

    Clean Test_VM
    Clean up test_vm before moving on to set up django

--- test_vm/bootstrap.sh
+++ test_vm/bootstrap.sh
@@ -2,12 +2,3 @@
 
 # Bash commands to run during VM initialization
 
-# ==== INSTALL & CONFIGURE TMUXIFIER ====
-# Tmuxifier allows me to start a tmux configuration from a script
-# This way we can run the jupyter and ganache servers conveniently 
-# in a tmux background process.
-
-# Install Tmux
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
-export PATH="$HOME/.tmuxifier/bin:$PATH"
-echo 'eval "$(tmuxifier init -)"' >> ~/.bashrc
--- test_vm/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-tmux
-# tmuxifier load-session jupyter_ganache
-tmuxifier load-window jupyter_ganache
