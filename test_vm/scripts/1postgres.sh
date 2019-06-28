eaa14f729387816fbf2a33bb992c84de71c98138
Arno <angerer.arno@gmail.com>
2019-06-28 17:40:21 +1000

    Clean up test_vm
    All scripts are integrated with prepare_system and setup_database. Delete individual script files

--- test_vm/scripts/1postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# ==== INSTALL POSTGRESQL ====
-
-sudo apt-get --assume-yes install postgresql postgresql-contrib libpq-dev 
-
-# This one can be merged with prepare_system.sh
--- test_vm/scripts/4django.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# ==== START DJANGO SERVER ====
-
-python /vagrant/django/manage.py runserver 0.0.0.0:8000
-
-# This can be included in run_.. server scripts as well as tmux script (run this server in new pane/window in tmux)
