e897c5399a4184a019c686ad3f584a3c332ee60e
Arno <angerer.arno@gmail.com>
2019-06-30 19:16:29 +1000

    Add error handling

--- django_vm/try_django/blog/views.py
+++ django_vm/try_django/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 from .models import BlogPost
 
 def blog_post_detail_page(request, post_id):
-	obj = BlogPost.objects.get(id=post_id) # query -> database -> data
+	try:
+		obj = BlogPost.objects.get(id=post_id) # query -> database -> data
+	except:
+		raise Http404
 	template_name 	= 'blog_post_detail.html'
 	# Pass in object into context
 	context 		= {"object": obj}
