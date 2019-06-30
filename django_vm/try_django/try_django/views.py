58f1a59895bd5516b3325e1314e7eb0951cb6fdc
Arno <angerer.arno@gmail.com>
2019-06-30 16:38:00 +1000

    Implemented base.html and template inheritance

--- django_vm/try_django/try_django/views.py
+++ django_vm/try_django/try_django/views.py
@@ -5,13 +5,13 @@ from django.shortcuts import render
 # Python function acts as view
 # View receives a request and returns a response
 def home_page(request):
-	my_title = "Hello there..."
+	my_title = "Dynamic Title"
 	# doc = "<h1>{title}</h1>".format(title=my_title)
 	# django_rendered_doc = "<h1>{{title}}</h1>".format(title=my_title)
 	return render(request, "hello_world.html", {"title": my_title})
 
 def about_page(request):
-	return render(request, "hello_world.html", {"title": "About us"})
+	return render(request, "about.html", {"title": "About us"})
 
 def contact_page(request):
-	return render(request, "hello_world.html", {"title": "Contact us"})
+	return render(request, "contact.html", {"title": "Contact us"})
