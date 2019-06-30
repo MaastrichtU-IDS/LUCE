fa504b1f92af29f1a8df919d8d9619063213269e
Arno <angerer.arno@gmail.com>
2019-06-30 15:55:53 +1000

    Include first template
    * create HTML template
    * use `render` in views to render template
    * set template folder in settings.py

--- django_vm/try_django/try_django/views.py
+++ django_vm/try_django/try_django/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+# Render allows us to render HTML templates
+from django.shortcuts import render
 
 # Python function acts as view
 # View receives a request and returns a response
 def home_page(request):
-	# function body creates content of view
-	return HttpResponse("<h1>Hello World</h1>")
+	return render(request, "hello_world.html")
 
 def about_page(request):
 	return HttpResponse("<h1>About Page</h1>")
