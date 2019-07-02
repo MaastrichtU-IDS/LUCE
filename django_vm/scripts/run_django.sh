fc84cd1cebbe5246e30c8f93467e521f178a0bdd
Arno <angerer.arno@gmail.com>
2019-07-02 17:38:11 +1000

    Include option for --noreload of Django server

--- django_vm/scripts/run_django.sh
+++ django_vm/scripts/run_django.sh
@@ -11,4 +11,7 @@ conda activate django_vm
 # Run server
 python /vagrant/try_django/manage.py runserver 0.0.0.0:8000
 
+# If server keeps restarting itself due to file changes
+#python /vagrant/try_django/manage.py runserver 0.0.0.0:8000 --noreload
+
 # This can be included in run_.. server scripts as well as tmux script (run this server in new pane/window in tmux)
