98fe0752097de28f3be8d55d1b41f65e660f6822
Arno <angerer.arno@gmail.com>
2019-07-02 14:28:57 +1000

    Final preparations for sharing the repository
    At this point all steps taken should be easy to follow

--- django_vm/scripts/bootstrap.sh
+++ django_vm/scripts/bootstrap.sh
@@ -2,9 +2,5 @@
 
 # Bash commands to run during VM initialization
 
-# Customise motd
-rm /etc/update-motd.d/*
-cp /vagrant/.etc/luce_motd /etc/update-motd.d/00-header
-
 # Copy scripts to vagrant user directory
 cp /vagrant/scripts/* /home/vagrant
