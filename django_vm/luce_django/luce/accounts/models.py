e21579f9e856e47dabefdb799d6cb2e023c99518
Arno <angerer.arno@gmail.com>
2019-07-09 18:01:44 +1000

    Create Custom UserManager

--- django_vm/luce_django/luce/accounts/models.py
+++ django_vm/luce_django/luce/accounts/models.py
@@ -5,6 +5,67 @@ from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, institution, ethereum_public_key=None, password=None, is_staff=False, is_admin=False):
+        """
+        Creates and saves a User with the given arguments and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not first_name:
+            raise ValueError('Users must have a first_name')
+        if not last_name:
+            raise ValueError('Users must have a last_name')
+        if not institution:
+            raise ValueError('Users must have an institution')
+        if not password:
+        	raise ValueError('Users must have a password')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.first_name 	= first_name
+        user.last_name 		= last_name
+        user.institution 	= institution
+        user.ethereum_public_key = ethereum_public_key
+        user.staff = is_staff
+        user.admin = is_admin
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, first_name, last_name, institution, password):
+        """
+        Creates and saves a staff user.
+        """
+        user = self.create_user(
+            email,
+            first_name,
+            last_name,
+            institution,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, institution, password):
+        """
+        Creates and saves a superuser.
+        """
+        user = self.create_user(
+            email,
+            first_name,
+            last_name,
+            institution,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
 
 # Our custom user class
 class User(AbstractBaseUser):
@@ -20,6 +81,7 @@ class User(AbstractBaseUser):
     first_name 	= 	models.CharField(max_length=255, blank=True, null=True)
     last_name 	= 	models.CharField(max_length=255, blank=True, null=True)
     institution =	models.CharField(max_length=255, blank=True, null=True)
+    ethereum_public_key 	=	models.CharField(max_length=255, blank=True, null=True)
 
     # active user? -> can login
     active = models.BooleanField(default=True)
@@ -33,6 +95,8 @@ class User(AbstractBaseUser):
     # USERNAME_FIELD & Password are required by default
     # Add additional required fields here:
     REQUIRED_FIELDS = ['first_name', 'last_name', 'institution'] 
+
+    objects = UserManager()
 
     def get_full_name(self):
         # The user is identified by their email address
